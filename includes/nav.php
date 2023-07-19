8945ba44b22ece74c999f6f5216d93e11734f812
Habeeb4shittu <habeeb4shittu@gmail.com>
2023-07-19 14:09:16 +0100

    faq and nav change

--- includes/nav.php
+++ includes/nav.php
@@ -3,11 +3,32 @@
         <img src="../assets/images/logos/Blue Transparent2.png" alt="">
     </a>
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
+        <li class="dropdown"><a href="#">Services</a>
+            <div class="dropdown-menu">
+                <div>
+                    <h4>Courses</h4>
+                    <ul>
+                        <li><a href="#">Software Engineering</a></li>
+                        <li><a href="#">Robotics & AI</a></li>
+                        <li><a href="#">Cyber Security</a></li>
+                        <li><a href="#">Product Design</a></li>
+                        <li><a href="#">Digital marketing</a></li>
+                        <li><a href="#">Data Science</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Services</h4>
+                    <ul>
+                        <li><a href="#">Hire a talent</a></li>
+                        <li><a href="#">Request A Service</a></li>
+                    </ul>
+                </div>
+            </div>
+        </li>
+
         <li><a href="#">Contact</a></li>
-        <li><a href="#">Courses</a></li>
         <li><a href="#">Blog</a></li>
         <a href="https://www.cohort.klemweb.com" class="apply-link">
             Apply Now
--- index.php
+++ index.php
@@ -11,74 +11,7 @@
 
     <body>
         <?php require_once __DIR__ . './includes/nav.php' ?>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, voluptatibus fugit iure incidunt eos quidem
-        blanditiis, reprehenderit distinctio tenetur officia a! Obcaecati sunt fugit cum porro debitis quos explicabo
-        consequuntur corrupti minima atque accusamus cumque in dolor harum, ipsa laborum qui, totam, vel pariatur!
-        Impedit, corrupti nemo? Quia, sapiente! Ex rem distinctio itaque odio atque accusantium optio. Voluptate nihil
-        reprehenderit iusto nobis sequi numquam suscipit optio veniam, incidunt nemo tempora quis, omnis impedit cumque
-        nulla. Cum nihil veniam illum alias deleniti corporis at a repellat. Fuga obcaecati qui dicta, in ipsam suscipit
-        delectus nemo, cumque fugiat dolorum sed eligendi. Perspiciatis voluptatum et cum corporis ut reprehenderit
-        sequi unde totam, maxime commodi. Minus animi dolorum ab ipsam accusamus eaque nam iure eius magni aliquam vitae
-        sunt, repellendus voluptas, ex hic modi sed cum, cumque quisquam est nostrum? Consectetur suscipit odio iure,
-        error sint sed assumenda dolor dicta rerum, non laborum enim ea voluptas eius atque illo inventore accusamus
-        hic. Quas optio dolorem molestiae delectus, asperiores dignissimos, fugit iure natus esse sint veniam, quasi
-        magni et sed libero consequuntur cupiditate neque dolore. Ea veniam, est consectetur sint dolores vitae animi
-        aperiam voluptatem? Sunt dicta recusandae aspernatur deserunt, ipsa nam explicabo debitis exercitationem
-        blanditiis pariatur quos laudantium tenetur, est nisi maxime. Ipsa alias reiciendis consequatur et laboriosam
-        expedita, pariatur beatae voluptas, asperiores quaerat saepe assumenda quia. Vero recusandae consequatur atque
-        earum nisi, nulla, nobis rerum quaerat dignissimos officia laudantium? Totam reprehenderit amet, ut at fugit
-        quisquam minima nam quasi nobis sapiente culpa velit magni doloremque ipsum soluta accusamus quo nostrum illo
-        quia harum aliquid veniam non eos perferendis! Exercitationem quis incidunt omnis facilis dignissimos aliquid
-        molestias quae. Amet nihil est sit quos. Doloribus eaque dicta assumenda vero. Necessitatibus, quaerat facilis
-        fugiat ratione tempora quam, minima officia fugit harum totam error obcaecati iure repellat voluptatem molestiae
-        saepe eos explicabo provident! Voluptates atque recusandae nam? Ducimus nobis adipisci, libero quaerat, saepe
-        obcaecati deleniti id sapiente aliquid similique, officiis reiciendis pariatur cupiditate quasi rem doloremque
-        quis cumque a ad et. Id voluptate perspiciatis quis amet deleniti eaque error pariatur eligendi, assumenda quam,
-        ex aperiam libero commodi. Excepturi reiciendis est omnis doloremque aliquam qui? Molestiae accusantium impedit
-        suscipit minima sapiente, nesciunt ratione enim dolorem officiis cupiditate deserunt dolore totam sint.
-        Cupiditate soluta vero, necessitatibus est delectus laboriosam quas quam doloremque deleniti distinctio esse
-        fuga dignissimos autem obcaecati numquam hic fugiat alias maxime sequi atque. Ex ad a illo repellendus quaerat
-        eaque debitis qui deleniti tenetur consectetur quod voluptate nulla facere omnis necessitatibus, quasi ut
-        eveniet harum architecto. Eius dolore, minima necessitatibus eos distinctio cumque perferendis nisi labore
-        nostrum porro, dicta voluptatibus illo inventore corporis repellat assumenda quod. Quidem reprehenderit
-        accusamus, corporis cum iusto similique maxime incidunt nisi consequuntur tempore ut dolorem consectetur itaque
-        voluptatem voluptate. Dolorum debitis mollitia eos sint soluta repellendus atque ipsam qui reprehenderit.
-        Perspiciatis, voluptate reprehenderit error consectetur in saepe a voluptas suscipit aliquid sit earum quidem
-        temporibus deserunt eos cumque nam dicta quia. Alias atque nulla beatae, fugit illo accusamus eos. Asperiores
-        eius labore impedit vero doloremque tenetur magnam deleniti praesentium, saepe ipsum porro ut voluptas unde
-        totam commodi magni, ab consectetur. Explicabo enim quisquam cumque corporis qui nisi magni labore adipisci,
-        quam corrupti id distinctio necessitatibus provident aspernatur quidem dolorem tenetur nemo suscipit est
-        deleniti. Soluta totam expedita ipsam deserunt est ipsa nulla modi nobis voluptatibus, veniam magni, doloribus
-        culpa quas hic vel vitae laborum odio! Nisi in, mollitia, tenetur natus porro nam autem, corrupti consequatur
-        nostrum veritatis provident repellat dolorem corporis? Porro unde itaque quod nemo ut hic incidunt, nostrum
-        deserunt rerum rem impedit temporibus perferendis voluptas aliquid libero consequuntur maxime laudantium
-        assumenda quam delectus sed natus ab eveniet. Eos ipsum possimus quaerat totam nihil fugiat id aperiam error
-        quod assumenda, fugit numquam, unde non earum quo eum? Fugit quaerat dolor hic dolorem error mollitia quo
-        voluptatibus, cum ex rerum amet quidem sit quas saepe earum odio, velit accusantium? Tempora officia quo atque
-        commodi aliquam autem adipisci asperiores ipsum dolor eum in perspiciatis libero, iure, id quis perferendis
-        error aut? Expedita autem, soluta illo iusto totam obcaecati laboriosam eaque exercitationem ipsum nam illum
-        voluptatum iste assumenda aperiam dolorem numquam! Odit atque ipsum repudiandae repellat enim. Ratione, laborum!
-        Reprehenderit ad quae ipsam fuga architecto dolorem laborum iure natus magnam illum, perspiciatis eligendi
-        facilis delectus neque omnis fugit eveniet accusamus repellendus et quam? Voluptatem culpa eveniet nemo,
-        deleniti magnam numquam fugiat vitae corrupti iure odio suscipit reiciendis modi magni alias nam qui, tempore
-        exercitationem? Perspiciatis doloribus porro maxime suscipit doloremque ex temporibus et alias pariatur. Amet
-        hic consectetur delectus, id ducimus nostrum? Illum maxime quo sapiente aperiam beatae eaque atque expedita iure
-        porro nulla debitis ipsa sint tempore, minus ut totam. Esse officiis quidem commodi quibusdam tempore at
-        corrupti praesentium error eligendi dolor dicta nostrum, earum quae perferendis, suscipit ad vero! Vero illum
-        quae quibusdam aperiam aliquam obcaecati unde nobis accusamus sint a, architecto repellat delectus blanditiis
-        enim accusantium commodi! Eveniet officia nesciunt deserunt aperiam quia neque tempore, iusto saepe mollitia
-        sapiente soluta voluptatibus odit beatae deleniti eum quas sequi illo molestias ipsam maiores ad tenetur
-        repudiandae quaerat. Aliquid, dolorem. Qui quisquam magnam laboriosam officia velit, repudiandae deserunt,
-        animi, quaerat blanditiis dolorem cupiditate! Quae dolorem nisi molestiae. Alias odio neque doloribus, ad unde
-        numquam excepturi obcaecati quis aliquid in omnis quia natus iure animi, ut non vel cumque, vero eum repellendus
-        soluta architecto commodi totam veritatis. Nobis, recusandae atque? Esse a cumque atque eius non praesentium
-        quisquam veritatis numquam porro. Tempora optio nisi itaque fugit perspiciatis praesentium dolorem at earum,
-        ratione excepturi exercitationem obcaecati facere modi et mollitia amet a? Accusantium, dolor totam laboriosam
-        provident quod sapiente ipsum dolore. Quod minus nobis totam aliquam consequatur et nostrum nesciunt quis beatae
-        ea, ipsam quidem impedit veritatis unde odio, explicabo, officiis odit dignissimos laborum quas cumque fugit
-        corporis consectetur quia. Qui esse nostrum tempore culpa a, sapiente eligendi maiores, possimus ea modi neque
-        obcaecati aspernatur mollitia iure blanditiis nam at praesentium quae corrupti. Temporibus nihil ab asperiores
-        exercitationem tempore explicabo dolor nam cumque cupiditate accusantium?
+        <?php require_once __DIR__ . './includes/faq.php' ?>
         <?php require_once __DIR__ . './includes/footer.php' ?>
         <?php require_once __DIR__ . './includes/scripts.php' ?>
     </body>
